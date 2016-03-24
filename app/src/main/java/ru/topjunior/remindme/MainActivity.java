a460a1865ad6c9547ef4745a72573ebff082a065
Vadim <basos2013>
2016-03-24 18:02:12 +0200

    Added MainActivity.java activity_main.xml Add in AndroidManifest root

---
+++ app/src/main/java/ru/topjunior/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.topjunior.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vadim on 24.03.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
